9945613a30f9929d3443b081e6e98ba19d2fd15e
NguyenDiep2001 <53925990+NguyenDiep2001@users.noreply.github.com>
2019-08-09 10:10:52 +0700

    bt9

---
+++ Assignment_7.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int kiem_tra_so_lon_nhat (int n){
+	int a[i];
+	int max = a[0];
+	for(i=0;i<n;i++)
+	if (max < a[i])
+		max = a[i];
+		
+		return max;
+}
+
+int main(){
+	int a[i]={1,2,3,4,5,6};
+	int n;
+	printf("so lon nhat trong mang la:%d\n",n,kiem_tra_so_lon_nhat(n));
+	
+	return 0;
+}
